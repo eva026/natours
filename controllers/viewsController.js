088b2a9813e791f6bcb9c9b5a4e37c2dc8f8ef65
Eva Wang <eva026.wangwang@gmail.com>
2020-07-07 16:52:57 +0800

    Added alert message when book successfully

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -83,3 +83,13 @@ exports.getMyTours = catchAsync(async (req, res, next) => {
     tours,
   });
 });
+
+exports.alerts = (req, res, next) => {
+  const { alert } = req.query;
+
+  if (alert === 'booking') {
+    res.locals.alert =
+      "Your booking was successful! Please check your email for a confirmation. If your booking doesn't show up immediately, please come back later.";
+  }
+  next();
+};
--- public/js/index.js
+++ public/js/index.js
@@ -70,3 +70,7 @@ if (bookBtn) {
     bookTour(tourId);
   });
 }
+
+// Show alert
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 10);
