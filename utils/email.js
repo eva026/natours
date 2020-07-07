ecd6e659e24b326ae5c7075168d4b5c5b89c4b37
Eva Wang <eva026.wangwang@gmail.com>
2020-07-07 18:45:39 +0800

    Updated email.js

--- utils/email.js
+++ utils/email.js
@@ -7,7 +7,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Eva Wang <${process.env.EMAIL_FROM}>`;
+    this.from = `Eva Wang <${process.env.SENDGRID_EMAIL_FROM}>`;
   }
 
   newTransport() {
